4ee20698ee9a1796cce43eb3c76296df9a3a8e2f
salmariavinola <108047293+salmariavinola@users.noreply.github.com>
2024-12-12 14:21:30 +0700

    Create User.java

---
+++ User.java
@@ -0,0 +1,13 @@
+public class User {
+    protected String username;
+    protected String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+}
